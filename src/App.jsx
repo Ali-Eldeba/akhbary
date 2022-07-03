ff8fdbc64da01580033863c5548cb3e4c5ae7aa3
Ali-Eldeba <alieldeba375@gmail.com>
2022-07-03 14:42:56 +0200

    completing front structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <div className="flex justify-center items-ceneter h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-2xl">Hello يسطا</h1>
+      <p>روح اعمل شغل الباك اند يا مضحك</p>
     </div>
   );
 }
