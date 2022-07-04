9a6cea5f01ab920a794b4a045ba00a852cf2b758
Ali-Eldeba <alieldeba375@gmail.com>
2022-07-04 06:49:28 +0200

    adding react structure

--- backend/env/akhbary/mainapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.conf import settings
-# Create your views here.
-def home(request):
-    #print(settings.BASE_DIR)
-    
-    return render(request,template_name="index.html" ,context={"msg":"message"})
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-2xl">Hello يسطا</h1>
-      <p>روح اعمل شغل الباك اند يا مضحك</p>
-    </div>
-  );
-}
